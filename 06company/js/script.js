976b0ebbd576ae895f8a21c9f44a3722509330bf
soyeong0411 <2so092@naver.com>
2022-07-21 18:19:23 +0900

    0721

--- 06company/js/script.js
+++ 06company/js/script.js
@@ -1,15 +1,26 @@
 
-// first-slide
-var swiper = new Swiper(".first", {
-    loop: true,
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
+$(".menubg").hide();
 $(".h_MenuWrap>ul>li").hover(function(){
+    $(".menubg,.submenu").stop().slideDown();
     $(this).addClass("on");
 },function(){
+    $(".menubg,.submenu").stop().slideUp();
     $(this).removeClass("on");
 })
+
+
+
+
+
+
+
+
+// // first-slide
+// var swiper = new Swiper(".first", {
+//     loop: true,
+//     navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
