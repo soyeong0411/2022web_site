006b59b26f0c6067f9e52919f78893ba1942bc12
soyeong0411 <2so092@naver.com>
2022-07-22 18:17:10 +0900

    0722

--- 06company/js/script.js
+++ 06company/js/script.js
@@ -1,26 +1,46 @@
 
 $(".menubg").hide();
 $(".h_MenuWrap>ul>li").hover(function(){
-    $(".menubg,.submenu").stop().slideDown();
+    $(".menubg,.h_MenuWrap .submenu").stop().slideDown();
     $(this).addClass("on");
 },function(){
-    $(".menubg,.submenu").stop().slideUp();
+    $(".menubg,.h_MenuWrap .submenu").stop().slideUp();
     $(this).removeClass("on");
 })
 
+$(".toggle").click(function(){
+    $(".h_menuClone").css("left",0)
+    $(".m_MenuBg").show()
+})
+
+$(".m_MenuBg").click(function(){
+    $(".h_menuClone").css("left","-100%")
+    $(".m_MenuBg").hide()
+})
+
+$(".h_MenuWrap>ul").clone().appendTo(".m_Menu")
 
 
+$(".headerMenu>li>a").click(function(e){
+    e.preventDefault();
+
+    $(".headerMenu>li>a").parent().find(".submenu").stop().slideUp()
+    if($(this).hasClass("active")){
+        $(".headerMenu>li>a").removeClass("active")
+    }else{
+        $(this).parent().find(".submenu").stop().slideDown()  // .show
+        $(".headerMenu>li>a").removeClass("active")
+        $(this).addClass("active")
+    }
+})
 
 
-
-
-
-// // first-slide
-// var swiper = new Swiper(".first", {
-//     loop: true,
-//     navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
+//first-slide
+var swiper = new Swiper(".first", {
+    loop: true,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
