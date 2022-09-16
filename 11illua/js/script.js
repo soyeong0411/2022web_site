c4552252272ea086f7792ea96ad5ee5a2c87c96c
soyeong0411 <2so092@naver.com>
2022-09-16 18:16:52 +0900

    0916

---
+++ 11illua/js/script.js
@@ -0,0 +1,7 @@
+$(".fullpage").fullpage({
+    menu:".mainMenu",
+    anchors:["m1st","m2st","m3st","m4st","m5st"],
+    navigation:true,
+    navigationPosition:"fp-right",
+    responsiveWidth:900
+})
